29e3ee30ce658a902d30ab52420c26d0f3de18e6
Matthew Longshore <matthewlongshore12@gmail.com>
2023-05-05 14:50:39 -0400

    communities section fetches the correct info now

--- client/src/userviews/CommHome.js
+++ client/src/userviews/CommHome.js
@@ -5,12 +5,16 @@ import React, { useContext, useState, useEffect } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import Button from "@mui/material/Button";
-import { useRadioGroup } from "@mui/material";
+import { Box, Card, Avatar, CardActions, CardContent, CardHeader, CardMedia, Checkbox, Collapse, IconButton, Typography } from '@mui/material';
+import { ExpandMore, Favorite, FavoriteBorder } from '@mui/icons-material';
+
+
 
 function CommHome() {
   const [commData, setCommData] = useState([]);
-  const [usersData, setUsersData] = useState([]);
-  const [postsData, setPostsData] = useState([]);
+  const [commUsersData, setCommUsersData] = useState([]);
+  const [usersData, setUsersData] = useState([])
+  const [postsData, setPostsData] = useState([])
   const [displayUsers, setDisplayUsers] = useState(false)
   const [displayPosts, setDisplayPosts] = useState(true)
   const { user, setUser } = useContext(UserContext);
@@ -29,8 +33,8 @@ function CommHome() {
         const usersResponse = await fetch(
           `http://127.0.0.1:5555/communities/${id}/users`
         );
-        const usersData = await usersResponse.json();
-        setUsersData(usersData);
+        const usersDatacomm = await usersResponse.json();
+        setCommUsersData(usersDatacomm);
 
         const postsResponse = await fetch(
           `http://127.0.0.1:5555/communities/${id}/posts`
@@ -51,6 +55,20 @@ function CommHome() {
     };
     fetchCommunityData();
   }, [id]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5555/users');
+        const data = await response.json();
+        setUsersData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
 
   const handleDisplayUsers = () => {
     setDisplayUsers(true)
@@ -82,16 +100,70 @@ function CommHome() {
       <div className="postWrapper">
         { displayUsers && ( 
             <>
-            {usersData.map((user) => ( 
+            {commUsersData.map((user) => ( 
                 <div key={user.id}>{user.username}</div>
             ))}
             </>
         )}
         { displayPosts && (
             <>
-            {postsData.map((posts) => ( 
-                <div key={posts.id}>{posts.caption}</div>
-            ))}
+            {postsData.map((posts) => {
+              const users = usersData.find((users) => users.id === posts.user_id)
+              return (
+                <div key={posts.id}><Card sx={{ margin: 5 }}>
+                <CardHeader
+                avatar={
+                  <Box
+                    sx={{
+                      display: 'inline-block',
+                      width: '32px',
+                      height: '32px',
+                      backgroundImage: user && `url(${users.profile_picture})`,
+                      backgroundSize: 'cover',
+                      borderRadius: '50%',
+                      marginRight: '8px',
+                    }}
+                  />
+                }
+                title={posts.caption}
+                subheader={`"${users ? users.username : ''}"`}
+              />
+                <CardMedia component="img" image={posts.image} />
+      
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    {posts.date_posted}
+                  </Typography>
+                </CardContent>
+      
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <Checkbox
+                      color="secondary"
+                      icon={<FavoriteBorder />}
+                      checkedIcon={<Favorite />}
+                    />
+                  </IconButton>
+                  {/* THIS IS A SHARE BUTTON IF I GET TO IT I WILL ADD IT AND YOU WILL BE ABLE TO SHARE POSTS IN CHATS */}
+                  {/* <IconButton aria-label="share">
+                    <Checkbox
+                      icon={<ScreenShareIcon />}
+                      checkedIcon={<ScreenShareIcon />}
+                    />
+                  </IconButton> */}
+                  {/* THIS IS A PLACEHOLDER TO EXPAND TO COMMENTS PROBABLY */}
+                  <ExpandMore
+                    //  expand={expanded}
+                    //  onClick={handleExpandClick}
+                    //  aria-expanded={expanded}
+                    aria-label="show more"
+                  >
+                    {/* <ExpandMoreIcon /> */}
+                  </ExpandMore>
+                </CardActions>
+              </Card></div>
+              )
+                })}
             </>
         )}
       </div>
