50f61f1bf115d9e7f9b46a19de1d1855bbd2ebde
Matthew Longshore <matthewlongshore12@gmail.com>
2023-05-10 07:26:40 -0400

    Make Post form styled

--- client/src/userviews/MakePost.js
+++ client/src/userviews/MakePost.js
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
-import { Avatar, Button, ButtonGroup, TextField, Typography } from '@mui/material'
+import { Avatar, Button, ButtonGroup, TextField, Typography , IconButton } from '@mui/material'
 import { Box, Stack } from '@mui/system'
 import React, { useState, useContext, useEffect } from 'react'
 import { UserContext } from '../statekeeper/state';
 import { Navigate, useNavigate } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 
 
 const StyledBox = styled(Box)({
@@ -13,11 +15,11 @@ const StyledBox = styled(Box)({
   backgroundColor: "#1c1c1c",
   padding: "40px",
   borderRadius: "5px",
-  boxShadow: "0px 0px 10px 5px #1eff00",
+  boxShadow: "0px 0px 10px 5px #88d4c3",
 })
 
 const StyledHeader = styled(Typography)({
-  color: "#ffffff",
+  color: "#88d4c3",
   fontSize: "32px",
   fontWeight: "bold",
   marginBottom: "40px",
@@ -25,29 +27,30 @@ const StyledHeader = styled(Typography)({
 
 const StyledTextField = styled(TextField)({
   "& label.Mui-focused": {
-    color: "#1eff00",
+    color: "#88d4c3",
   },
   "& .MuiInput-underline:after": {
-    borderBottomColor: "#1eff00",
+    borderBottomColor: "#88d4c3",
   },
   "& .MuiOutlinedInput-root": {
     "& fieldset": {
       borderColor: "#ffffff",
     },
     "&:hover fieldset": {
-      borderColor: "#1eff00",
+      borderColor: "#88d4c3",
     },
     "&.Mui-focused fieldset": {
-      borderColor: "#1eff00",
+      borderColor: "#88d4c3",
     },
   },
 })
 
 const SubmitButton = styled(Button)(({ theme }) => ({
-  backgroundColor: '#29b6f6',
-  color: '#fff',
+  backgroundColor: '#88d4c3',
+  color: '#121212',
   '&:hover': {
-    backgroundColor: '#0086c3',
+    backgroundColor: '#88d4c3',
+    color: '#fff'
   },
 }));
 
@@ -89,49 +92,64 @@ const MakePost = () => {
       .catch((error) => console.error(error));
   }, []);
 
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
 
   return (
-    <StyledBox>
-      <StyledHeader>Create Post</StyledHeader>
-      <form onSubmit={handleSubmit} style={{width: "100%"}}>
-        <Box sx={{ display: "flex", alignItems: "center", gap: "10px", mb: 3 }}>
-          <Avatar src="" sx={{ width: 30, height: 30 }} />
-          <Typography fontWeight={500} variant="span" sx={{color: "#ffffff"}}>
-            {user.username}
-          </Typography>
-        </Box>
-        <StyledTextField
-          sx={{ width: "100%", mb: 2 }}
-          inputProps={{ style: { color: "#ffffff" } }}
-          id="image"
-          label="Image"
-          variant="outlined"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />
-          <StyledTextField
-            sx={{ width: "100%", mb: 2 }}
-            inputProps={{ style: { color: "#ffffff" } }}
-            multiline
-            id="caption"
-            label="Caption"
-            variant="outlined"
-            value={caption}
-            onChange={(e) => setCaption(e.target.value)}
-          />
-          <StyledTextField
-            sx={{ width: "100%", mb: 2 }}
-            inputProps={{ style: { color: "#ffffff" } }}
-            multiline
-            id="community"
-            label="Community"
-            variant="outlined"
-            value={community}
-            onChange={(e) => setCommunity(e.target.value)}
-          />
-          <SubmitButton type="submit">Post</SubmitButton>
-        </form>
-    </StyledBox>
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "20px", minHeight: "100vh", backgroundColor: "#141414" }}>
+      <header className='toppage'>
+      <IconButton onClick={handleGoBack} style={{ marginRight: '10px' }}>
+                <ArrowBackIcon />
+      </IconButton>
+      </header>
+      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "50px 0" }}>
+        <StyledBox>
+          <StyledHeader>Create Post</StyledHeader>
+          <form onSubmit={handleSubmit} style={{width: "100%"}}>
+            <Box sx={{ display: "flex", alignItems: "center", gap: "10px", mb: 3 }}>
+              <Avatar src="" sx={{ width: 30, height: 30 }} />
+              <Typography fontWeight={500} variant="span" sx={{color: "#ffffff"}}>
+                {user.username}
+              </Typography>
+            </Box>
+            <StyledTextField
+              sx={{ width: "100%", mb: 2 }}
+              inputProps={{ style: { color: "#ffffff" } }}
+              id="image"
+              label="Image"
+              variant="outlined"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+            />
+            <StyledTextField
+              sx={{ width: "100%", mb: 2 }}
+              inputProps={{ style: { color: "#ffffff" } }}
+              multiline
+              id="caption"
+              label="Caption"
+              variant="outlined"
+              value={caption}
+              onChange={(e) => setCaption(e.target.value)}
+            />
+            <StyledTextField
+              sx={{ width: "100%", mb: 2 }}
+              inputProps={{ style: { color: "#ffffff" } }}
+              multiline
+              id="community"
+              label="Community"
+              variant="outlined"
+              value={community}
+              onChange={(e) => setCommunity(e.target.value)}
+            />
+            <SubmitButton type="submit">Post</SubmitButton>
+          </form>
+        </StyledBox>
+      </Box>
+      <footer className='buttpage'>
+      </footer>
+    </Box>
   );
 };
 
