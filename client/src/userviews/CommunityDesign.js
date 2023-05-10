8dccc6b8a1f624b1e31d86be874f8fc97a4d6284
Matthew Longshore <matthewlongshore12@gmail.com>
2023-05-10 01:05:03 -0400

    Community Page Styled

--- client/src/userviews/CommunityDesign.js
+++ client/src/userviews/CommunityDesign.js
@@ -2,9 +2,11 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { ImageList, ImageListItem, ListSubheader, ImageListItemBar, IconButton } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom'
+import InfoIcon from '@mui/icons-material/Info';
+
 
 
 
@@ -33,32 +35,34 @@ const CommunityDesign = () => {
   return (
 
     <>
-    {commData.map((item) => {
-        return (
-        <Card sx={{ maxWidth: 345 }}
-        onClick={() => navigate(`/communities/${item.id}/home`)}>
-        <CardActionArea>
-            <CardMedia
-            component="img"
-            height="140"
-            image={item.image}
-            />
-            <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-                {item.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-                {item.description}
-            </Typography>
-            <Typography variant="body3" color="text.secondary">
-                {item.video_game}
-            </Typography>
-            </CardContent>
-        </CardActionArea>
-        </Card>
-        )
-
-        })}
+     <ImageList sx={{ width: 500, height: 450 }}>
+      {/* <ImageListItem key="Subheader" cols={2}>
+        <ListSubheader component="div">December</ListSubheader>
+      </ImageListItem> */}
+    {commData.map((item) => (
+        <ImageListItem key={item.id}>
+        <img
+          src={`${item.image}?w=248&fit=crop&auto=format`}
+          srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+          alt={item.name}
+          loading="lazy"
+        />
+        <ImageListItemBar
+          title={item.name}
+          subtitle={item.video_game}
+          actionIcon={
+            <IconButton
+              onClick={() => navigate(`/communities/${item.id}/home`)}
+              sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+              aria-label={`info about ${item.name}`}
+            >
+              <InfoIcon />
+            </IconButton>
+          }
+        />
+      </ImageListItem>
+    ))}
+  </ImageList>
     </>
   )
 }
--- client/src/userviews/CommunityPage.js
+++ client/src/userviews/CommunityPage.js
@@ -1,10 +1,11 @@
-import { Box, Fab, Tooltip } from '@mui/material';
+import { Box, Fab, Tooltip, Divider } from '@mui/material';
 import React, { useContext, useState } from "react";
 import { Navigate, useNavigate } from 'react-router-dom'
 import { UserContext } from '../statekeeper/state'
 import NavBar from './NavBar'
 import Sidebar from './Sidebar'
 import CommunityDesign from './CommunityDesign';
+import "../stylesheets/commpage.css"
 
 const CommunityPage = () => {
     const { user, setUser } = useContext( UserContext )
@@ -16,28 +17,42 @@ const CommunityPage = () => {
     }
 
     return (
-        <div>
+        <>
             <NavBar />
-            <Sidebar />
-            <Box flex={4} p={2}>
-                <Box display="inline-block">
-                    <CommunityDesign />
-                </Box>
-                <Tooltip
-                    onClick={() => navigate('/communities/add')}
-                    title="Add"
-                    sx={{
-                        position: "fixed",
-                        bottom: 20,
-                        left: { xs: "calc(50% - 25px)", md: 30 },
-                    }}
-                    >
-                    <Fab color="white" aria-label="add">
-                        <h1>+</h1>
-                    </Fab>
-                </Tooltip>
-            </Box>
-        </div>
+            <div className="community">
+                <Sidebar />
+                <div className="communityright">
+                    <h1>Discover Communities</h1>
+                    <Divider
+                        variant="middle"
+                        sx={{
+                            backgroundColor: "black",
+                            height: "2px",
+                            width: "100%",
+                            margin: "16px 0",
+                        }}
+                    />
+                    <Box flex={4} p={2}>
+                        <Box display="inline-block">
+                            <CommunityDesign />
+                        </Box>
+                        <Tooltip
+                            onClick={() => navigate('/communities/add')}
+                            title="Add"
+                            sx={{
+                                position: "fixed",
+                                bottom: 20,
+                                left: { xs: "calc(50% - 25px)", md: 30 },
+                            }}
+                            >
+                            <Fab color="white" aria-label="add">
+                                <h1>+</h1>
+                            </Fab>
+                        </Tooltip>
+                    </Box>
+                </div>
+            </div>
+        </>
     )
 }
 
